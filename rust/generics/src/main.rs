0ec9f6f8faac92ea0f6500af9fcb0037efac8b63
Jeff van Geete <jeff.vangeete@protectwise.com>
2019-03-18 17:45:33 -0600

    generics

--- rust/generics/src/main.rs
+++ rust/generics/src/main.rs
@@ -20,6 +20,24 @@ fn largest_char(list: &[char]) -> char {
     largest
 }
 
+#[derive(Debug)]
+struct Point<T, U> {
+    x: T,
+    // y: T // you can use a single T in the signature, or mix types is is shown
+    y: U,
+}
+
+// doesn't work yet
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     let result = largest_i32(&number_list);
@@ -28,4 +46,11 @@ fn main() {
     let char_list = vec!['y', 'm', 'a', 'q'];
     let result = largest_char(&char_list);
     println!("The largest char is {}", result);
+
+    // let integer = Point{x: 4, y: 5};
+    // let float = Point{x: 4.1, y: 5.3};
+
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
 }
