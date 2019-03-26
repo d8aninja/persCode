4acc282f162e349eb174fa0a4723b51ba16755f3
Jeff van Geete <jeff.vangeete@protectwise.com>
2019-03-26 08:15:30 -0600

    more illustrative if_let eg

--- rust/if_let/src/main.rs
+++ rust/if_let/src/main.rs
@@ -1,8 +1,62 @@
-fn main() {
-    let some_u8 = Some(0u8);
-    match some_u8 {
-        Some(3) = println!("Three!");
-        _ = (),
 
+enum Foo {
+    Bar, 
+    Baz,
+    Qux(u32),
+}
+
+fn main() {
+    let optional = Some(7);
+
+    match optional {
+        Some(i) => {
+            println!("This is a really long string and `{:?}`", i);
+        },
+        _ => {},
     }
+
+    // all i32
+    let number = Some(7);
+    let letter: Option<i32> = None;
+    let emoticon: Option<i32> = None;
+
+    if let Some(i) = number {
+        println!("Matched {:?}!", i);
+    } 
+    
+    if let Some(i) = letter {
+        println!("Matched {:?}!", i);
+    } else {
+        println!("Didn't match a number, let's go with a letter!")
+    }
+
+    let i_like_letters = false;
+    if let Some(i) = emoticon {
+        println!("Matched {:?}!", i);
+    } else if i_like_letters{
+        println!("Didn't match a number, let's go with a letter!")
+    } else {
+        println!("I don't like letters - let's go with an emoticon :D", )
+    }
+
+    let a = Foo::Bar;
+    let b = Foo::Baz;
+    let c = Foo::Qux(100);
+
+    if let Foo::Bar = a {
+        println!("a is foobar!");
+    }
+
+    if let Foo::Bar = b {
+        println!("b is foobar!");
+    } else {
+        println!("b is not foobar!");
+    }
+
+    // Similar to Some(...)
+    if let Foo::Qux(value) = c {
+        println!("c is fooqux and {}!", value);
+    }
+
+    
 }
