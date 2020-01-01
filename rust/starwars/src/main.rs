56e3d109bcbdad260eb7a95171b9ba0a3709d966
Jeff van Geete <jeff.vangeete@protectwise.com>
2020-01-01 16:52:16 -0700

    woohoo! 3/3 tests pass

--- rust/starwars/src/main.rs
+++ rust/starwars/src/main.rs
@@ -1,89 +1,19 @@
 //use cgmath::prelude::*;
-use rand::prelude::*;
-use s2::r3::vector::Vector as r3_vec;
-
 mod troops;
-use troops::{
-    Character,
-    Faction,
-    Weapon,
-};
+
+use troops::*;
+
 mod ships;
-use ships::{
-    Ship,
-    Ships,
-    Shoot,
-    can_travel,
-};
+
+use ships::*;
+
 mod space;
-use space::{
-    Star,
-    Planet,
-    System,
-};
+
+use space::*;
 
 fn main() {
-
-    let mut rng = rand::thread_rng();
-
-    let tatooine = Planet {
-        name: String::from("Tatooine"),
-        class: String::from("M"),
-        stage: String::from("Spez"),
-        radius: 10.5,
-        orbital: false,
-        loc: r3_vec{ x: 7.2, y: 3.4, z: 5.6 },
-    };
-    println!("{:?}\n", tatooine);
-
-    let red_five = Ship {
-        pilot: Some(luke),
-        kind: Some(Ships::XWing("AA-589".to_string(), Faction::Rebels)),
-        kills: 18,
-        fuel: 0.13,
-        health: 0.9,
-        in_flight: true,
-        grounded: false,
-        rebel_scum: true,
-        dmg: rng.gen(),
-        loc: r3_vec{x: 3.23, y: 4.61, z: 5.94},
-    };
-//    println!("{:?}", &red_five);
-    can_travel(&red_five);
-
-    let red_six = Ship {
-        pilot: Some(porkins),
-        kind: Some(Ships::XWing("AA-399".to_string(), Faction::Rebels)),
-        kills: 4,
-        fuel: 0.56,
-        loc: r3_vec{x: 3.13, y: 4.62, z: 5.95},
-        ..red_five
-    };
-//    println!("{:?}", &red_six);
-    can_travel(&red_six);
-
-    let tie_one = Ship {
-        pilot: Some(vader),
-        kind: Some(Ships::TIEBomber("TB-9".to_string(), Faction::Imperials)),
-        kills: 493,
-        fuel: 0.9,
-        health: 1.0,
-        in_flight: true,
-        grounded: false,
-        rebel_scum: false,
-        dmg: rng.gen(),
-        loc: r3_vec{x: 3.15, y: 4.55, z: 5.92}
-    };
-//    println!("{:?}", &tie_one);
-    can_travel(&tie_one);
-
-    tie_one.shoot(red_five.clone());
-    &red_five.shoot(tie_one.clone());
-    red_five.shoot(tie_one.clone());
-
-    println!("\n\n Distance from TIE One to RED Six: {}", &tie_one.loc.distance(&red_six.loc));
-
     /*
+    let mut rng = rand::thread_rng();
     rng & turbofish testing
     let n1: u8 = rng.gen();
     let n2: u16 = rng.gen();
@@ -98,33 +28,91 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use super::troops::{Character, Weapon};
+    use rand::prelude::*;
+    use s2::r3::vector::Vector as r3_vec;
+    use super::ships::*;
+    use super::space::*;
+    use super::troops::*;
 
     #[test]
-    fn ships() {}
+    fn test_troops() {
+        let alderaan = Planet::default();
+        let leia = Character {
+            name: String::from("Leia"),
+            home_planet: Some(alderaan),
+            weapon: Some(Weapon::Ranged),
+            dmg: Some(10.0),
+        };
+        assert_eq!(leia.name, "Leia".to_string())
+    }
 
     #[test]
-    fn space {}
+    //#[ignore]
+    fn test_ships() {
+        //deps on rng :(
+        let mut rng = rand::thread_rng();
+        let luke = Character::default();
+        let vader = Character::default();
+
+        let red_five = Ship {
+            pilot: Some(luke),
+            // Faction reaches across to troops mod...ok?
+            kind: Some(Ships::XWing("AA-589".to_string(), Faction::Rebels)),
+            kills: 18,
+            fuel: 13,
+            health: 9,
+            in_flight: 1,
+            grounded: 0,
+            rebel_scum: 1,
+            dmg: rng.gen::<f32>(),
+            loc: r3_vec { x: 3.23, y: 4.61, z: 5.94 },
+        };
+
+        let tie_one = Ship {
+            pilot: Some(vader),
+            kind: Some(Ships::TIEBomber("TB-9".to_string(), Faction::Imperials)),
+            kills: 493,
+            fuel: 90,
+            health: 100,
+            in_flight: 1,
+            grounded: 0,
+            rebel_scum: 0,
+            dmg: rng.gen::<f32>(),
+            loc: r3_vec { x: 3.15, y: 4.55, z: 5.92 },
+        };
+
+        let distance = travel_distance(&red_five);
+        assert_eq!(
+            distance,
+            130
+//            format!("{:?} ({:?}), from {:?} can travel a distance of {:?} light-years.",
+//                    &red_five.pilot.as_ref().unwrap().name,
+//                    &red_five.kind.as_ref().unwrap(),
+//                    &red_five.pilot.as_ref().unwrap().home_planet.name,
+//                    distance
+//            )
+        );
+
+        //todo: test shooting
+        //tie_one.shoot(red_five.clone());
+        //&red_five.shoot(tie_one.clone());
+        //red_five.shoot(tie_one.clone());
+
+        //todo: assertion; parameterize the names, like above
+        //println!("\n\n Distance from TIE One to RED Six: {}", &tie_one.loc.distance(&red_six.loc));
+    }
 
     #[test]
-    fn troops() {
-        let luke = Character {
-            name: String::from("Luke"),
-            home_planet: tatooine.clone(),
-            weapon: Weapon::Melee,
-            dmg: 10.0,
+    fn test_space() {
+        let tatooine = Planet {
+            name: String::from("Tatooine"),
+            class: Some(String::from("M")),
+            stage: Some(String::from("Spez")),
+            radius: Some(10.5),
+            orbital: Some(false),
+            loc: r3_vec { x: 7.2, y: 3.4, z: 5.6 },
         };
-        let porkins = Character {
-            name: String::from("Porkins"),
-            home_planet: tatooine.clone(),
-            weapon: Weapon::Melee,
-            dmg: 3.5,
-        };
-        let vader = Character {
-            name: String::from("Darth Vader"),
-            home_planet: tatooine.clone(),
-            weapon: Weapon::Melee,
-            dmg: 11.5,
-        };
+        assert_eq!(tatooine.name, "Tatooine".to_string())
     }
 }
+
